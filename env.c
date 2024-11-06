b8317dbda88a3373e35bd1a7cf9deddbcc9aeb8f
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2024-11-06 13:05:46 +0100

    Put error in the stderr

--- env.c
+++ env.c
@@ -6,7 +6,7 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 11:19:57 by ansebast          #+#    #+#             */
-/*   Updated: 2024/11/06 11:32:03 by ansebast         ###   ########.fr       */
+/*   Updated: 2024/11/06 13:04:19 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	ft_env(t_command *cmd, char **env)
 
 	if (cmd->args)
 	{
-		printf("env: No options or arguments should be passed\n");
+		ft_putstr_fd("env: No options or arguments should be passed\n", 2);
 		return (1);
 	}
 	i = 0;
