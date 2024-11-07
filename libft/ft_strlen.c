fd04e42f4ee79f67eb1560d84ac96822aeea8c28
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2024-11-07 21:25:31 +0100

    Update libft functions

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 11:31:44 by ansebast          #+#    #+#             */
-/*   Updated: 2024/08/03 08:32:42 by ansebast         ###   ########.fr       */
+/*   Updated: 2024/11/06 14:44:24 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ size_t	ft_strlen(const char *str)
 	size_t	len;
 
 	len = 0;
+	if (!str)
+		return (0);
 	while (str[len] != '\0')
 		len++;
 	return (len);
