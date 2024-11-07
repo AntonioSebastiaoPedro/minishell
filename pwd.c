bb69132498835ad4d330948caa94eecda6210acf
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2024-11-07 21:34:17 +0100

    Fix norminette errors

--- pwd.c
+++ pwd.c
@@ -6,7 +6,7 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 01:37:03 by ansebast          #+#    #+#             */
-/*   Updated: 2024/11/05 01:38:24 by ansebast         ###   ########.fr       */
+/*   Updated: 2024/11/07 20:42:00 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	ft_pwd(void)
 {
 	char	cwd[1024];
+
 	if (getcwd(cwd, sizeof(cwd)) != NULL)
 		printf("%s\n", cwd);
 	else
