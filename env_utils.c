62c0b8723ce7955e5b22c2ab42f369824832b7b8
Antonio Sebastiao Pedro <antoniosebastiaopedro19@gmail.com>
2024-11-07 21:32:11 +0100

    Separate env functions utils

---
+++ env_utils.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env_utils.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/06 14:17:28 by ansebast          #+#    #+#             */
+/*   Updated: 2024/11/07 19:50:10 by ansebast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	update_index(t_env *head)
+{
+	int		index;
+	t_env	*current;
+
+	index = 0;
+	current = head;
+	while (current != NULL)
+	{
+		current->index = index;
+		index++;
+		current = current->next;
+	}
+}
+
+t_env	*add_env(t_env **envs, char *name)
+{
+	t_env	*new_env;
+	char	*var;
+	char	*value;
+
+	new_env = malloc(sizeof(t_env));
+	if (!new_env)
+	{
+		perror("malloc");
+		return (NULL);
+	}
+	var = strtok(name, "=");
+	value = strtok(NULL, "=");
+	new_env->var = var;
+	new_env->value = value;
+	if (!var)
+		new_env->var = ft_strdup("");
+	if (!value)
+		new_env->value = ft_strdup("");
+	new_env->next = *envs;
+	*envs = new_env;
+	update_index(*envs);
+	return (new_env);
+}
+
+void	free_env(t_env **env)
+{
+	t_env	*current;
+	t_env	*next_node;
+
+	current = *env;
+	while (current != NULL)
+	{
+		next_node = current->next;
+		free(current->var);
+		if (current->value)
+			free(current->value);
+		free(current);
+		current = next_node;
+	}
+	*env = NULL;
+}
+
+void	envcpy(t_env **env_dup, char **src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		add_env(env_dup, src[i]);
+		i++;
+	}
+}
