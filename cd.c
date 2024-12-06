6cf841ca91f2e475cf8f5fb8c2492e07cb222c10
Antonio Teca <ateca@c1r1s3.42luanda.com>
2024-12-06 12:26:44 +0100

    imprimir mensagem de forma correcta

--- cd.c
+++ cd.c
@@ -12,18 +12,27 @@
 
 #include "minishell.h"
 
+void	print_error_cd(char *path);
+
 int	ft_cd(char **args, t_env **env)
 {
 	char	*path;
 	char	*cwd;
+	char	*msg_err;
 
-	path = get_env_value("HOME", env);
+	path = NULL;
 	if (args)
 		if (args[0])
 			path = args[0];
-	if (chdir(path) == -1)
+	if (!path)
 	{
-		printf("%s: ", strerror(errno));
+		msg_err = "minishell: cd: with only a relative or absolute path\n";
+		write(2, msg_err, ft_strlen(msg_err));
+		return (1);
+	}
+	else if (chdir(path) == -1)
+	{
+		print_error_cd(path);
 		return (1);
 	}
 	cwd = getcwd(NULL, 0);
@@ -31,4 +40,22 @@ int	ft_cd(char **args, t_env **env)
 	update_envvar(env, "PWD", cwd);
 	free(cwd);
 	return (0);
+}
+
+void	print_error_cd(char *path)
+{
+	char	*program_name;
+	char	*colon_space;
+	char	*newline;
+	char	*error_message;
+
+	program_name = "minishell: cd: ";
+	colon_space = ": ";
+	newline = "\n";
+	error_message = strerror(errno);
+	write(2, program_name, ft_strlen(program_name));
+	write(2, path, ft_strlen(path));
+	write(2, colon_space, ft_strlen(colon_space));
+	write(2, error_message, ft_strlen(error_message));
+	write(2, newline, ft_strlen(newline));
 }
