5c553318931d9c6a1faf92c9680a3cb582e74706
António Sebastião Pedro <antoniosebastiaopedro19@outlook.com>
2025-01-09 10:49:17 +0100

    Fix wrong response when the argument is too long

--- libft/ft_isint.c
+++ libft/ft_isint.c
@@ -6,7 +6,7 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/01 00:41:25 by ansebast          #+#    #+#             */
-/*   Updated: 2025/01/09 10:10:41 by ansebast         ###   ########.fr       */
+/*   Updated: 2025/01/09 10:46:06 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,13 @@ int	ft_islong(char *str)
 {
 	char	*endptr;
 	long	val;
+	int	i;
 
+	i = 0;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	if (ft_strlen(&str[i]) > 19 || ft_strcmp("9223372036854775808", &str[i]) < 0)
+		return (0);
 	if (*str == '\0' || (*str == '-' && *(str + 1) == '\0') || (*str == '+'
 			&& *(str + 1) == '\0'))
 		return (0);
