6a2f73a7945cb928d3f446961e24c1534bf1718f
António Sebastião Pedro <antoniosebastiaopedro19@outlook.com>
2025-01-09 10:19:01 +0100

    Exit with long values

--- libft/ft_isint.c
+++ libft/ft_isint.c
@@ -6,7 +6,7 @@
 /*   By: ansebast <ansebast@student.42luanda.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/01 00:41:25 by ansebast          #+#    #+#             */
-/*   Updated: 2024/09/01 00:41:59 by ansebast         ###   ########.fr       */
+/*   Updated: 2025/01/09 10:10:41 by ansebast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,20 @@ int	ft_isint(char *str)
 		return (0);
 	val = ft_strtol(str, &endptr);
 	if (*endptr != '\0' || val < -2147483648 || val > 2147483647)
+		return (0);
+	return (1);
+}
+
+int	ft_islong(char *str)
+{
+	char	*endptr;
+	long	val;
+
+	if (*str == '\0' || (*str == '-' && *(str + 1) == '\0') || (*str == '+'
+			&& *(str + 1) == '\0'))
+		return (0);
+	val = ft_strtol(str, &endptr);
+	if (*endptr != '\0' || val < LONG_MIN || val > LONG_MAX)
 		return (0);
 	return (1);
 }
